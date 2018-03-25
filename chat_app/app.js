bc560410b495cb8ee4521eb2666da723090cdc7e
Jaewon Choi <qwebnm7788@naver.com>
2018-03-26 00:22:28 +0900

    귓속말 기능 추가

--- chat_app/app.js
+++ chat_app/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const port = 3000;
 
-var socket_ids = [];
+var socket_ids = [];                //nickname to socket_id mapping을 위한 배열
 var count = 0;
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,32 +16,31 @@ io.sockets.on('connection', (socket) => {
     registerUser(socket, 'GUEST-' + count);
     count++;
 
-    socket.on('changename', (data) => {
+    socket.on('changename', (data) => {                 //이름이 변경된 경우 새롭게 등록 후 userlist로 업데이트 시킨다.
         registerUser(socket, data.nickname);
     });
 
-    socket.on('disconnect', (data) => {
-        socket.get('nickname', (err, nickname) => {
-            if(nickname != undefined) {
-                delete socket_ids[nickname];
-                io.sockets.emit('userlist', {users: Object.keys(socket_ids)});
-            }
-        });
+    socket.on('disconnect', (data) => {                 //사용자가 브라우저를 종료하는 경우
+        var nickname = socket.nickname;                 //현재 사용자의 이름을 읽어 온뒤 리스트에서 삭제한다.
+        if(nickname != undefined) {
+            delete socket_ids[nickname];
+            io.sockets.emit('userlist', {users: Object.keys(socket_ids)});          //현재 종료하는 소켓을 제외한 모든 사용자 소켓에게 userlist 이벤트를 발생하여 제거된 사용자를 제외한 새로운 리스트로 업데이트 시킨다.
+        }
     });
 
-    socket.on('send_msg', (data) => {
-        socket.get('nickname', (err, nickname) => {
-            data.msg = nickname + ' : ' + data.msg;
-            if(data.to == 'ALL') socket.broadcast.emit('broadcast_msg', data);
-            else {
-                socket_id = socket_ids[data.to];
-                if(socket_id != undefined) {
-                    io.sockets.socket(socket_id).emit('broadcast_msg', data);
-                }
+    socket.on('send_msg', (data) => {                       //메세지 전송 시
+        var nickname = socket.nickname;
+
+        data.msg = nickname + ' : ' + data.msg;
+        if(data.to == 'ALL') socket.broadcast.emit('broadcast_msg', data);          //전체 메세지 전송시 모든 소켓에게 이벤트를 발생시키고
+        else {
+            socket_id = socket_ids[data.to];                                        //그 외의 경우 귓속말 기능으로 상대방 소켓에게만 이벤트를 발생시킨다.
+            if(socket_id != undefined) {
+                io.to(socket_id).emit('broadcast_msg', data);
             }
-            socket.emit('broadcast_msg', data);
-        })
-    })
+        }
+        socket.emit('broadcast_msg', data);
+    });
 });
 
 server.listen(port, () => {
@@ -49,11 +48,9 @@ server.listen(port, () => {
 });
 
 function registerUser(socket, nickname) {
-    socket.get('nickname', (err, pre_nick) => {             //현재 소켓이 가지고 있는 nick_name을 pre_nick으로 받아옴
-        if(pre_nick != undefined) delete socket_ids[pre_nick];          //기존의 값 삭제
-        socket_ids[nickname] = socket.id;
-        socket.set('nickname', nickname, () => {
-            io.sockets.emit('userlist', {users: Object.keys(socket_ids)});
-        });
-    });
+    var pre_nick = socket.nickname;                                     //현재 소켓이 가지고 있는 nick_name을 pre_nick으로 받아옴
+    if(pre_nick != undefined) delete socket_ids[pre_nick];              //기존의 값 삭제
+    socket.nickname = nickname;                                         //현재 소켓에 nickname 속성 설정
+    socket_ids[socket.nickname] = socket.id;
+    io.sockets.emit('userlist', {users: Object.keys(socket_ids)});          //현재 접속된 사용자의 리스트를 전송 (내가 추가된) -> nickname을 key로 저장해두었기 떄문에 value가 아닌 key로 전달한다.(value로는 socket_id가 들어있음)
 }
--- chat_app/public/javascript/index.js
+++ chat_app/public/javascript/index.js
@@ -1,13 +1,13 @@
 var socket = io.connect('http://localhost:3000');
 
 $("#changename").click(() => {
-    socket.emit('changename', {nickname: $("#nickname").val()});
+    socket.emit('changename', {nickname: $("#nickname").val()});                //이름이 변경된 경우 changename 이벤트 발생 (변경된 이름을 함께 전송)
 });
 
 $("#msgbox").keyup((event) => {                 //키보드를 뗄때 이벤트 발생
     if(event.which === 13) {                    //엔터 키일 때 지금까지의 값을 전송
-        socket.emit('fromclient', {msg: $("#msgbox").val()});
-        $("#msgbox").val('');
+        socket.emit('send_msg', {to: $('#to').val(), msg: $('#msgbox').val()});             //선택된 상대방에 대한 정보와 메세지 전달
+        $("#msgbox").val('');                                                               //현재 값 초기화
     }
 });
 
@@ -20,15 +20,15 @@ socket.on('userlist', (data) => {
     var users = data.users;
     console.log(users);
     console.log(data.users.length);
-    $('#to').empty().append('<option value="ALL">ALL</option>');
-    for(let i = 0; i < data.users.length; i++) {
+    $('#to').empty().append('<option value="ALL">ALL</option>');            //지우고 새로운 option (ALL) 태그를 생성한다.
+    for(let i = 0; i < data.users.length; i++) {                            //전달된 사용자 수 만큼 option 태그(각 사용자의 nickname이 들어간)를 추가한다.
         $('#to').append('<option value="' + users[i] + '">' + users[i] + "</option>");
     }
 });
 
 socket.on('broadcast_msg', (data) => {
     console.log(data.msg);
-    $("#msgs").append(data.msg + '<br>');
+    $("#msgs").append(data.msg + '<br>');                               //전달된 메세지 추가
 });
 
 socket.on('toclient', (data) => {               //toclient 이벤트 발생시 화면에 출력
